28d852ce46513616d48ef35b2b9f0a1a98fd15bb
Liam Kikuchi <liamshinx@gmail.com>
2017-03-15 18:37:12 -0700

    Added image to Metal(Normal) Block.
    Also added some placeholder code for future addition to block types.  Warning; I have not used switch cases much, let alone in JS, so don't yell at me if I got something wrong.

--- entities/block.js
+++ entities/block.js
@@ -1,11 +1,16 @@
-var Block = function (x, y, width, height)/*When creating a new instance of the block class, you need to pass in the position you want it to have*/{
+var Block = function (x, y, type){/*When creating a new instance of the block class, you need to pass in the position you want it to have*/
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
+    this.type = type;//Available types include: Normal(N), Temp(T), and Spike(S).  For type, just enter the initial of the type of block you want.
+    switch(type){
+        case "N":
+          var image = "https://cdn.discordapp.com/attachments/219666140098461696/291741990830473216/Metal_BlockNormal.png";
+          break;
+        case "T":
+          var image =""; 
+    }
 };
 
-Block.prototype.draw = function () {
-  c.fillstyle = "#000080" //navy blue
-  c.fillRect(this.x,this.y,this.width,this.height)
+Block.prototype.draw = function (){
+  c.drawImage(image, 0, 0, 32, 32, x, x, 32, 32);
 };

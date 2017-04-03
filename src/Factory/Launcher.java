b0bcfa706e1938a5646468b82fdd334e3cac3ea1
JunhaLee <junhalee02@gmail.com>
2017-04-02 19:41:44 -0700

    Changed Resolution

--- src/Factory/Launcher.java
+++ src/Factory/Launcher.java
@@ -1,16 +1,10 @@
-//This is but a test
+
 package Factory;
-//This is but a test part 2
-import java.awt.Dimension;
-import java.awt.Toolkit;
 
 public class Launcher {
 	
 	public static void main(String [] args){	
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		double width = screenSize.getWidth();
-		double height = screenSize.getHeight();
-		Game game = new Game("The Factory", (int)width, (int)height);
+		Game game = new Game("The Factory", 1280, 720);
 		game.start();	}
 }
 	

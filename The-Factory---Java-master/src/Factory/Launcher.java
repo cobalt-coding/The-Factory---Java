52f44b9823ccfd7290b5d6678a0e44ba3e117341
Lemon <liamshinx@gmail.com>
2017-04-02 19:15:24 -0700

    hi

---
+++ The-Factory---Java-master/src/Factory/Launcher.java
@@ -0,0 +1,17 @@
+//This is me testing a thing
+package Factory;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class Launcher {
+	
+	public static void main(String [] args){	
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		Game game = new Game("The Factory", (int)width, (int)height);
+		game.start();	}
+}
+	
+    

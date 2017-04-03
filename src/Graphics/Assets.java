7421fdec2aefd499c1b5c4c5e60453deaccfa953
Lemon <liamshinx@gmail.com>
2017-04-02 20:32:32 -0700

    Made some Image loading classes

---
+++ src/Graphics/Assets.java
@@ -0,0 +1,17 @@
+package Graphics;
+
+import java.awt.image.BufferedImage;
+
+public class Assets {
+
+	private static final int  width = 50, height = 50;
+	
+	//Add images to the "res" folder and initiate a variable on the line below as a BufferedImage
+	
+	//public static BufferedImage
+	
+	static void init(){
+		
+		// exampleImage.Imageloader.loadImage("/textures/exampleImage.png");
+	}
+}

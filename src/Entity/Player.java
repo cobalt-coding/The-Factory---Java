d4f38698d4befede15010e3fbe51c7e870f0e521
JunhaLee <junhalee02@gmail.com>
2017-04-16 17:22:49 -0700

    velocities/buggy collision yay

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -9,6 +9,8 @@ public class Player extends Entity {
 
 	private Game game;
 	
+	private float velx = 0, vely = 0;
+	
 	public Player(Game game, float x, float y) {
 		super(x, y);
 		this.game = game;
@@ -19,25 +21,41 @@ public class Player extends Entity {
 		if(game.getKeyManager().up){
 			//Jump physics will have to be put here
 			if(y>0){
-				y-=3;
+				vely-=2.1;
+			} else {
+				vely = 0;
+				y = 0;
 			}
 		}	
 		if(game.getKeyManager().down){
 			//only will be used with ladders...maybe a crouch
 			if(y<685){
-				y+=3;
+				vely+=2.1;
+			} else {
+				vely = 0;
+				y = 685;
 			}
 		}	
 		if(game.getKeyManager().left){
 			if(x>0){
-				x-=3;
+				velx-=2.1;
+			} else {
+				velx = 0;
+				x = 0;
 			}
 		}	
 		if(game.getKeyManager().right){
 			if(x<1233){
-				x+=3;
+				velx+=2.1;
+			} else {
+				velx = 0;
+				x = 1233;
 			}
 		}
+		x+=this.velx;
+		y+=this.vely;
+		this.velx/=1.2;
+		this.vely/=1.2;
 	}
 
 	@Override
